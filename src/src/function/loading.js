e5bc90f5735480e94eccc0c19a9f729547f6412e
IVoynelovych <deafreak89@gmail.com>
2023-09-17 14:26:03 +0300

    dt

--- src/src/function/loading.js
+++
@@ -1,8 +0,0 @@
-const loader = document.querySelector('.loader');
-document.body.style.overflow = 'hidden';
-loader.style.display = 'block';
-
-setTimeout(() => {
-  document.body.style.overflow = '';
-  loader.style.display = 'none';
-}, 5000); 
